67d9ff61a9a1a3fa461eceabfb420e878be65c8d
thiswind <thiswind@gmail.com>
2016-11-07 16:43:09 +0800

    add readme.md

--- core/reverse_proxy/start.sh
+++ core/reverse_proxy/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #re-gen reverse proxy list
-python gen_hosts.py > etc/nginx/conf.d/auto_gen_reverse_proxy.conf
+python gen_reverse_proxy_conf.py > ./etc/nginx/conf.d/auto_gen_reverse_proxy.conf
 
 #clean start nginx
 docker-compose up -d
