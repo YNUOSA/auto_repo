795bcb32b0880843d57a10b6b4fe2707e6a371bd
STRRL <str_ruiling@outlook.com>
2017-01-01 22:44:18 +0800

    add centos

---
+++ repo_gears/centos/start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#
+# Created by thiswind on 2016/11/9.
+#
+
+# re-fresh glboal value
+
+local_dir="$(cat ../NFS_LOCAL_DIR)"
+deepin_dir="$local_dir/centos"
+
+mkdir -p $deepin_dir
+
+# gen docker-compose.yml
+python gen_docker_compose.py > docker-compose.yml
+
+
+# start docker
+docker-compose build && docker-compose up -d 
--- repo_gears/start_all.sh
+++ repo_gears/start_all.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
-#cd /home/strrl/auto_repo/repo_gears/deepin_archive
-#sh start.sh &
-#cd /home/strrl/auto_repo/repo_gears/deepin_release
-#sh start.sh &
+!/bin/sh
+cd /home/strrl/auto_repo/repo_gears/deepin_archive
+sh start.sh &
+cd /home/strrl/auto_repo/repo_gears/deepin_release
+sh start.sh &
 cd /home/strrl/auto_repo/repo_gears/kali
+sudo sh start.sh &
+cd /home/strrl/auto_repo/repo_gears/centos
 sudo sh start.sh &
