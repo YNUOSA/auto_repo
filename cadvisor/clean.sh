888f40b7670992e64f08b0643b3420c95982fd3d
STRRL <str_ruiling@outlook.com>
2017-01-09 18:16:29 +0800

    add cadvisor in Makefile

---
+++ cadvisor/clean.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo docker stop cadvisor && sudo docker rm cadvisor
---
+++ cadvisor/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:rw \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --publish=8089:8080 \
+  --detach=true \
+  --name=cadvisor \
+  google/cadvisor:latest
