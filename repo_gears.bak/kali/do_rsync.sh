cd5f7dc704a01550384a32ddcb85f340ea659934
strrl <str_ruiling@outlook.com>
2017-01-21 22:23:51 +0800

    del bakup file

--- repo_gears.bak/kali/do_rsync.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#
-# Created by thiswind on 2016/11/9.
-#
-
-#rsync -av --delete-after rsync.deepin.com::deepin/ /var/www/deepin/
-#rsync -v -r --delete-after rsync.deepin.com::releases/ /var/www/deepin/
-rsync -av --delete-after rsync://mirrors.tuna.tsinghua.edu.cn/kali/ /var/www/deepin
--- repo_gears.bak/start_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /home/strrl/auto_repo/repo_gears/deepin
-sh start.sh &
-cd /home/strrl/auto_repo/repo_gears/deepin_release
-sh start.sh &
-cd /home/strrl/auto_repo/repo_gears/kali
-sudo sh start.sh &
-cd /home/strrl/auto_repo/repo_gears/centos
-sudo sh start.sh &
